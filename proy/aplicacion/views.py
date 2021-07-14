1049c04d6066b1a390c8c41102ee0ca8c613aa77
C2H5OH-hic <62526342+C2H5OH-hic@users.noreply.github.com>
2021-07-14 03:13:40 -0400

    a

--- proy/aplicacion/views.py
+++ proy/aplicacion/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def holaa(request):
-    return render(request, 'holaa.html', {'name': 'Camilo'})
 
+
+# Aquí es para hacer la llamada hacia una plantilla html
+def index(request):
+    return render(request, 'aplicacion/inicio.html', {'name': 'Camilo'})
+
+# Aquí es para hacer la llamada desde un texto hecho aquí.
 def comoestas(request):
     return HttpResponse('hola oceano')
 
